c9d9b235a329594e433cc83795b9143acc2ce5f7
root <root@ip-172-31-15-83.ap-northeast-1.compute.internal>
2016-09-29 07:46:22 +0000

    update install script

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from os import path
 import os
 import pip
 import stat
+import site
+from distutils.sysconfig import get_python_lib
 
 model_list = ['Arabic', 'Basque', 'Bulgarian', 'Catalan', 'Chinese', 'Croatian',
     'Czech', 'Danish', 'Dutch', 'English', 'Estonian', 'Finnish', 'French',
@@ -17,23 +19,11 @@ model_list = ['Arabic', 'Basque', 'Bulgarian', 'Catalan', 'Chinese', 'Croatian',
 
 class InstallClass(install):
     def run(self):
-        pip.main(['install', 'https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl'])
         install.run(self)
-        package_dir = [p for p in sys.path if p.endswith('site-packages')][0]
-        package_dir = path.join(package_dir, 'syntaxnet_wrapper')
-        subprocess.call(['tar', 'xzf', 'models.tgz'], cwd=package_dir)
-        model_dir = path.join(package_dir, 'models/syntaxnet/syntaxnet/models/parsey_universal')
-        model_url = 'http://download.tensorflow.org/models/parsey_universal/{}.zip'
-        for model_name in model_list:
-            subprocess.call(['wget', '-q', model_url.format(model_name)], cwd=model_dir)
-            subprocess.call(['unzip', '-n', '-qq', '{}.zip'.format(model_name)], cwd=model_dir)
-            os.unlink(path.join(model_dir, '{}.zip'.format(model_name)))
-            dir_name = path.join(model_dir, model_name)
-            os.chmod(dir_name, os.stat(dir_name).st_mode | stat.S_IROTH | stat.S_IXOTH)
-            for fn in os.listdir(dir_name):
-                fpath = path.join(dir_name, fn)
-                os.chmod(fpath, os.stat(fpath).st_mode | stat.S_IROTH)
-            print model_name, 'model installed.'
+        sys.path.reverse()
+        import syntaxnet_wrapper
+        syntaxnet_wrapper_dir = syntaxnet_wrapper.__path__[0]
+        subprocess.call(['make'], cwd=syntaxnet_wrapper_dir)
 
 
 setup(name='syntaxnet_wrapper',
@@ -55,5 +45,5 @@ setup(name='syntaxnet_wrapper',
       test_suite='nose.collector',
       tests_require=['nose'],
       include_package_data=True,
-      package_data={'syntaxnet_wrapper': ['models.tgz']}
+      package_data={'syntaxnet_wrapper': ['models.tgz', 'makefile']}
 )
