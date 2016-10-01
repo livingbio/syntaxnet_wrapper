a9b7bd9f621bece095bb222cc13e494e902c0c09
banyh <banyhong@gliacloud.com>
2016-10-01 05:25:53 +0000

    add install_tar command

--- setup.py
+++ setup.py
@@ -26,8 +26,32 @@ class InstallClass(install):
         subprocess.call(['make'], cwd=syntaxnet_wrapper_dir)
 
 
+class InstallTarClass(install):
+    def run(self):
+        pip.main(['install', 'https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl'])
+        install.run(self)
+        package_dir = [p for p in sys.path if p.endswith('site-packages')][0]
+        package_dir = path.join(package_dir, 'syntaxnet_wrapper')
+        print 'Start downloading syntaxnet.tgz...'
+        subprocess.call(['wget', '-q', 'https://gliacloud.blob.core.windows.net/nlp/syntaxnet.tgz'], cwd=package_dir)
+        print 'Start extracting syntaxnet.tgz...'
+        subprocess.call(['tar', 'xzf', 'syntaxnet.tgz'], cwd=package_dir)
+        model_dir = path.join(package_dir, 'models/syntaxnet/syntaxnet/models/parsey_universal')
+        model_url = 'http://download.tensorflow.org/models/parsey_universal/{}.zip'
+        print 'Start downloading pretrained models...'
+        for model_name in model_list:
+            subprocess.call(['wget', '-q', model_url.format(model_name)], cwd=model_dir)
+            subprocess.call(['unzip', '-n', '-qq', '{}.zip'.format(model_name)], cwd=model_dir)
+            os.unlink(path.join(model_dir, '{}.zip'.format(model_name)))
+            dir_name = path.join(model_dir, model_name)
+            os.chmod(dir_name, os.stat(dir_name).st_mode | stat.S_IROTH | stat.S_IXOTH)
+            for fn in os.listdir(dir_name):
+                fpath = path.join(dir_name, fn)
+                os.chmod(fpath, os.stat(fpath).st_mode | stat.S_IROTH)
+            print model_name, 'model is installed.'
+
 setup(name='syntaxnet_wrapper',
-      version='0.3.0',
+      version='0.3.1',
       description='A Python Wrapper for Google SyntaxNet',
       url='https://github.com/livingbio/syntaxnet_wrapper',
       author='Ping Chu Hung',
@@ -41,9 +65,10 @@ setup(name='syntaxnet_wrapper',
       ],
       cmdclass={
           'install': InstallClass,
+          'install_tar': InstallTarClass,
       },
       test_suite='nose.collector',
       tests_require=['nose'],
       include_package_data=True,
-      package_data={'syntaxnet_wrapper': ['models.tgz', 'makefile']}
+      package_data={'syntaxnet_wrapper': ['makefile']}
 )
