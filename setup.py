c75cd2b82b83b663e837d2a1c2a1f72c218dcd05
banyh <banyhong@gliacloud.com>
2017-02-20 12:52:51 +0800

    v0.4.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class InstallClass(install):
 
 
 setup(name='syntaxnet_wrapper',
-      version='0.3.2',
+      version='0.4.0',
       description='A Python Wrapper for Google SyntaxNet',
       url='https://github.com/livingbio/syntaxnet_wrapper',
       author='Ping Chu Hung',
