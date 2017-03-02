55e45f4648afb6b5ffc046b193116514f26431f0
george <goblin_george@gmail.com>
2017-03-03 00:52:41 +0800

    update

--- test.py
+++ test.py
@@ -7,9 +7,9 @@
 
 from syntaxnet_wrapper import tagger, parser
 
-assert tagger['en'].query('this is a good day', returnRaw=True)
-assert parser['en'].query('Alice drove down the street in her car', returnRaw=True)
-
-assert tagger['zh'].query(u'今天 天氣 很 好', returnRaw=True)
-assert parser['zh'].query(u'今天 天氣 很 好', returnRaw=True)
-
+tag = tagger['en'].query('this is a good day', returnRaw=True)
+assert tag
+print tag
+p = parser['en'].query('Alice drove down the street in her car', returnRaw=True)
+assert p
+print p
