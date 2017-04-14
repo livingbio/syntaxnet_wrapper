8ee92f00fd4488ac3c58cc66e7b6850ae0f86adc
banyh <banyhong@gliacloud.com>
2017-04-14 05:28:39 +0000

    first version of dragnn wrapper

---
+++ syntaxnet_wrapper/dragnn_parse_forever.py
@@ -0,0 +1,180 @@
+from __future__ import unicode_literals, print_function
+import re
+import sys
+import signal
+from os import path
+
+import tensorflow as tf
+
+from google.protobuf import text_format
+from tensorflow.python.platform import gfile
+
+from dragnn.protos import spec_pb2
+from dragnn.python import graph_builder
+from dragnn.python import spec_builder
+from syntaxnet import sentence_pb2
+from syntaxnet.util import check
+
+import dragnn.python.load_dragnn_cc_impl
+import syntaxnet.load_parser_ops
+
+
+def length_to_bytes(length):
+    if length <= 0x7f:
+        return [length]
+    else:
+        return [length % 0x80 + 0x80, (length - 0x80) // 0x80 + 1]
+
+
+def gen_char_corpus(sentence):
+    char = bytearray([18])
+    byte_sent = sentence.encode('utf8')
+    len_sent = len(byte_sent)
+    char.extend(length_to_bytes(len_sent))
+    char.extend(byte_sent)
+    pos = 0
+    for ch in sentence:
+        byte_ch = ch.encode('utf8')
+        chlen = len(byte_ch)
+        end_pos = pos + chlen - 1
+        itemlen = 6 + chlen + int(pos > 0x7f) + int(end_pos > 0x7f)
+
+        char.extend([26, itemlen, 10, chlen])
+        char.extend(byte_ch)
+        char.append(16)
+        char.extend(length_to_bytes(pos))
+        char.append(24)
+        char.extend(length_to_bytes(end_pos))
+        pos += chlen
+    return bytes(char)
+
+
+def gen_seg_corpus(words, sep=' '):
+    sentence = sep.join(words)
+    char = bytearray([18])
+    byte_sent = sentence.encode('utf8')
+    char.extend(length_to_bytes(len(byte_sent)))
+    char.extend(byte_sent)
+    pos = 0
+    non_first_word = 0
+    for w in words:
+        byte_word = w.encode('utf8')
+        len_word = len(byte_word)
+        end_pos = pos + len_word - 1
+        itemlen = 8 + len_word + int(pos > 0x7f) + int(end_pos > 0x7f)
+
+        char.extend([26, itemlen, 10, len_word])
+        char.extend(byte_word)
+        char.append(16)
+        char.extend(length_to_bytes(pos))
+        char.append(24)
+        char.extend(length_to_bytes(end_pos))
+        char.append(64)
+        char.append(non_first_word)
+        non_first_word = 1
+        pos += len_word + len(sep)
+    return bytes(char)
+
+
+def post_parse(processed_sentence):
+    parsed_sents = []
+    for serialized_sentence in processed_sentence:
+        sentence = sentence_pb2.Sentence()
+        sentence.ParseFromString(serialized_sentence)
+        sent = []
+        for i, token in enumerate(sentence.token):
+            item = {'pos': '_', 'ptb': '_'}
+            for field, value in sorted(token.ListFields())[::-1]:
+                if field.json_name == 'tag':
+                    attrs = re.findall('(\w+): +\"([\w\+]+)\" +(\w+): +\"([\w\+\$]+)\"', token.tag)
+                    for _, name, _, val in attrs:
+                        if name == 'fPOS':
+                            pos, ptb = val.split('++')
+                            item['pos'] = pos
+                            item['ptb'] = ptb
+                        else:
+                            item[name] = val
+                elif field.json_name == 'word':
+                    item['name'] = value
+                else:
+                    if field.json_name == 'label' and value == 'root':
+                        item['head'] = -1
+                    if field.json_name == 'label' and value == 'punct':
+                        item['pos'] = 'PUNCT'
+                        item['ptb'] = item['name'] if 'name' in item else '_'
+                    item[field.json_name] = value
+            sent.append(item)
+        parsed_sents.append(sent)
+    return parsed_sents
+
+
+if __name__ == '__main__':
+    model_name = sys.argv[1]
+    basedir = path.join(path.dirname(__file__), 'models', 'syntaxnet')
+    exec_dir = path.join(basedir, 'bazel-bin', 'dragnn', 'tools', 'parse-to-conll.runfiles', '__main__')
+    model_dir = path.join(basedir, 'dragnn', 'conll17', model_name)
+    seg_dir = path.join(basedir, 'dragnn', 'conll17', model_name, 'segmenter')
+    assert path.isdir(model_dir)
+    assert path.isdir(seg_dir)
+    assert path.isdir(exec_dir)
+
+    parser_master_spec = path.join(model_dir, 'parser_spec.textproto')
+    parser_checkpoint_file = path.join(model_dir, 'checkpoint')
+    parser_resource_dir = model_dir
+    segmenter_master_spec = path.join(seg_dir, 'spec.textproto')
+    segmenter_checkpoint_file = path.join(seg_dir, 'checkpoint')
+    segmenter_resource_dir = seg_dir
+    component_beam_sizes = [('char_lstm', '16'), ('lookahead', '16'),
+                            ('tagger', '32'), ('parser', '64')]
+
+    master_spec = spec_pb2.MasterSpec()
+    with gfile.FastGFile(parser_master_spec) as fin:
+        text_format.Parse(fin.read(), master_spec)
+    spec_builder.complete_master_spec(master_spec, None, parser_resource_dir)
+
+    g = tf.Graph()
+    with g.as_default(), tf.device('/device:CPU:0'):
+        hyperparam_config = spec_pb2.GridPoint()
+        hyperparam_config.use_moving_average = True
+        builder = graph_builder.MasterBuilder(master_spec, hyperparam_config)
+        annotator = builder.add_annotation()
+        builder.add_saver()
+
+    sess = tf.Session(graph=g)
+    with g.as_default():
+        with sess.as_default():
+            sess.run(tf.global_variables_initializer())
+            sess.run('save/restore_all', {'save/Const:0': parser_checkpoint_file})
+
+    def stdin_handler(signum, frame):
+        line = sys.stdin.readline()[:-1].decode('utf8')
+        char_corpus = [gen_seg_corpus(line.split())]
+        feed_dict = {annotator['input_batch']: char_corpus}
+        for comp, beam_size in component_beam_sizes:
+            feed_dict['%s/InferenceBeamSize:0' % comp] = beam_size
+        results = sess.run(annotator['annotations'], feed_dict=feed_dict)
+        parsed = post_parse(results)
+
+        sys.stdout.write('\n## result start\n')
+        sys.stdout.flush()
+        for sent in parsed:
+            for i, word in enumerate(sent):
+                conll = '\t'.join([str(i + 1), word['name'], '_', word['pos'], word['ptb'], '_',
+                                   str(word['head'] + 1), word['label'], '_', '_'])
+                print(conll)
+            print()
+        sys.stdout.write('\n## result end\n')
+        sys.stdout.flush()
+
+    def abort_handler(signum, frame):
+        sess.close()
+        sys.exit(0)
+
+    signal.signal(signal.SIGALRM, stdin_handler)
+    signal.signal(signal.SIGABRT, abort_handler)
+
+    while True:
+        sys.stdout.write('\n## input content:\n')
+        sys.stdout.flush()
+        signal.alarm(1800)
+        signal.pause()
---
+++ syntaxnet_wrapper/syntaxnet_class.py
@@ -0,0 +1,246 @@
+import subprocess
+import os
+import stat
+import six
+import signal
+
+from fcntl import fcntl, F_SETFL, F_GETFD
+import time
+from datetime import datetime, timedelta
+import logging
+from os.path import join, dirname, abspath
+
+logger = logging.getLogger()
+pwd = dirname(abspath(__file__))
+PIDFILE_PATH = os.path.join(pwd, 'pids')
+
+
+class TimeoutException(Exception):
+    pass
+
+
+class SyntaxNetWrapper(object):
+
+    def __del__(self):
+        self.stop()
+
+    def clean_zombie_process(self):
+        for pidfile in os.listdir(PIDFILE_PATH):
+            if not pidfile.endswith('.pid') or pidfile.count('_') != 2:
+                continue
+            pid, model, clsname = pidfile.split('_')
+            try:
+                os.kill(int(pid), 0)
+                os.unlink(os.path.join(PIDFILE_PATH, pidfile))
+            except:
+                logger.info('kill zombie process {}'.format(pid))
+                self.kill_process(pidfile)
+
+    def kill_process(self, pidfile):
+        try:
+            with open(os.path.join(PIDFILE_PATH, pidfile)) as f:
+                pid = f.read().strip()
+                try:
+                    os.kill(int(pid), 9)
+                except Exception as e:
+                    logger.info(e)
+            os.unlink(os.path.join(PIDFILE_PATH, pidfile))
+        except Exception as e:
+            logger.info(e)
+
+    def make_pidfile(self):
+        if not os.path.isdir(PIDFILE_PATH):
+            os.mkdir(PIDFILE_PATH)
+        pidfilename = os.path.join(PIDFILE_PATH, "{}.pid".format(self.name))
+        for fn in os.listdir(PIDFILE_PATH):
+            if not fn.endswith('.pid') or fn.count('_') != 2:
+                continue
+            pid, model, clsname = fn.split('_')
+            if clsname == self.__class__.__name__ + '.pid' and model == self.model_name:
+                self.kill_process(fn)
+        with open(pidfilename, 'w+') as f:
+            f.write(str(self.process.pid))
+        os.chmod(pidfilename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
+
+    @property
+    def name(self):
+        return u"{}_{}_{}".format(os.getpid(), self.model_name, self.__class__.__name__)
+
+    def start(self):
+        rundir = join(pwd, 'models/syntaxnet/bazel-bin/syntaxnet/parser_eval.runfiles/__main__')
+        command = ['python', self.run_filename, self.model_path, self.context_path]
+
+        env = os.environ.copy()
+        env['PYTHONPATH'] = rundir
+        subproc_args = {'stdin': subprocess.PIPE, 'stdout': subprocess.PIPE,
+                        'stderr': subprocess.STDOUT, 'cwd': pwd,
+                        'env': env, 'close_fds': True}
+        self.process = subprocess.Popen(command, shell=False, **subproc_args)
+        self.out = self.process.stdout
+        self.din = self.process.stdin
+        fcntl(self.out.fileno(), F_SETFL, fcntl(self.out.fileno(), F_GETFD) | os.O_NONBLOCK)
+        self.make_pidfile()
+
+    def stop(self):
+        self.din.close()
+        try:
+            import signal  # wordaround for AttributeError("'NoneType' object has no attribute 'SIGTERM'",)
+            os.kill(self.process.pid, signal.SIGTERM)
+            self.process.send_signal(signal.SIGTERM)
+            self.process.kill()
+            self.process.wait()
+        except OSError:
+            pass
+
+    def __init__(self, run_filename, model_name):
+
+        self.model_name = model_name
+        self.run_filename = run_filename
+
+        if model_name == 'English-Parsey':
+            model_path = 'models/syntaxnet'
+            context_path = 'models/syntaxnet/syntaxnet/models/parsey_mcparseface/context.pbtxt'
+        elif model_name == 'ZHTokenizer':
+            model_path = 'models/syntaxnet/syntaxnet/models/parsey_universal/Chinese'
+            context_path = 'models/syntaxnet/syntaxnet/models/parsey_universal/context-tokenize-zh.pbtxt'
+        else:
+            model_path = 'models/syntaxnet/syntaxnet/models/parsey_universal/{!s}'.format(model_name)
+            context_path = 'models/syntaxnet/syntaxnet/models/parsey_universal/context.pbtxt'
+
+        context_path = join(pwd, context_path)
+        model_path = join(pwd, model_path)
+
+        self.model_path = model_path
+        self.context_path = context_path
+
+        self.start()
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def wait_for(self, text, timeout=5):
+        result = []
+        start_time = datetime.now()
+        while True:
+            try:
+                line = self.out.readline().decode('utf-8').strip()
+                if text == line:
+                    return result
+                result.append(line)
+            except:
+                # read timeout
+                time.sleep(0.1)
+            finally:
+                now = datetime.now()
+                if(now - start_time) > timedelta(0, timeout):
+                    raise TimeoutException()
+
+    def __query(self, text, returnRaw=False):
+        self.wait_for('## input content:')
+
+        # push data
+        self.din.write(text.encode('utf8') + six.b('\n'))
+        self.din.flush()
+        self.process.send_signal(signal.SIGALRM)
+
+        self.wait_for('## result start')
+        results = self.wait_for('## result end')
+
+        if returnRaw:
+            return '\n'.join(results).strip() + "\n"
+        return [r.split('\t') for r in results[:-2]]
+
+    def query(self, text, returnRaw=False):
+        for i in xrange(3):
+            try:
+                return self.__query(text, returnRaw)
+            except Exception:
+                # retart process
+                self.restart()
+
+    def list_models(self):
+        pwd = dirname(abspath(__file__))
+        model_path = os.path.join(pwd, 'models/syntaxnet/syntaxnet/models/parsey_universal')
+        files = os.listdir(model_path)
+        models = []
+        for fn in files:
+            if os.path.isdir(os.path.join(model_path, fn)):
+                models.append(fn)
+        models.append('English-Parsey')
+        return sorted(models)
+
+
+class SyntaxNetTokenizer(SyntaxNetWrapper):
+
+    def __init__(self, model_name='ZHTokenizer'):
+        super(SyntaxNetTokenizer, self).__init__('tokenizer_eval_forever.py', model_name)
+
+    def query(self, text):
+        return super(SyntaxNetTokenizer, self).query(text, returnRaw=True)
+
+
+class SyntaxNetMorpher(SyntaxNetWrapper):
+
+    def __init__(self, model_name='English'):
+        if model_name == 'Chinese':
+            self.tokenizer = SyntaxNetTokenizer()
+        else:
+            self.tokenizer = None
+        super(SyntaxNetMorpher, self).__init__('morpher_eval_forever.py', model_name)
+
+    def query(self, text, returnRaw=False):
+        if self.tokenizer:
+            tokenized_text = self.tokenizer.query(text)
+        else:
+            tokenized_text = text
+        return super(SyntaxNetMorpher, self).query(tokenized_text, returnRaw)
+
+    def query_raw(self, tokenized_text, returnRaw=False):
+        return super(SyntaxNetMorpher, self).query(tokenized_text, returnRaw)
+
+
+class SyntaxNetTagger(SyntaxNetWrapper):
+
+    def __init__(self, model_name='English-Parsey', **kwargs):
+        if model_name == 'English-Parsey':
+            self.morpher = None
+        elif 'morpher' in kwargs:
+            self.morpher = kwargs['morpher']
+        else:
+            self.morpher = SyntaxNetMorpher(model_name)
+        super(SyntaxNetTagger, self).__init__('tagger_eval_forever.py', model_name)
+
+    def query(self, morphed_text, returnRaw=False):
+        if self.morpher:
+            conll_text = self.morpher.query(morphed_text, returnRaw=True)
+        else:
+            conll_text = morphed_text
+        return super(SyntaxNetTagger, self).query(conll_text, returnRaw)
+
+    def query_raw(self, conll_text, returnRaw=False):
+        return super(SyntaxNetTagger, self).query(conll_text, returnRaw)
+
+
+class SyntaxNetParser(SyntaxNetWrapper):
+
+    def __init__(self, model_name='English-Parsey', **kwargs):
+        if 'tagger' in kwargs:
+            self.tagger = kwargs['tagger']
+            self.morpher = self.tagger.morpher
+        else:
+            if model_name == 'English-Parsey':
+                self.morpher = None
+            elif 'morpher' in kwargs:
+                self.morpher = kwargs['morpher']
+            else:
+                self.morpher = SyntaxNetMorpher(model_name)
+            self.tagger = SyntaxNetTagger(model_name, morpher=self.morpher)
+        super(SyntaxNetParser, self).__init__('parser_eval_forever.py', model_name)
+
+    def query(self, text, returnRaw=False):
+        conll_text = self.tagger.query(text, returnRaw=True)
+        return super(SyntaxNetParser, self).query(conll_text, returnRaw)
+
+    def query_raw(self, conll_text, returnRaw=False):
+        return super(SyntaxNetParser, self).query(conll_text, returnRaw)
